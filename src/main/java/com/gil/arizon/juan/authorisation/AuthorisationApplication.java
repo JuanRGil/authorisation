d3e778ad2dbacfc7b2436ea8e6336bed153d82a5
JuanGilArizon <juangilarizon@gmail.com>
2020-04-25 18:40:15 +0200

    first commit

---
+++ src/main/java/com/gil/arizon/juan/authorisation/AuthorisationApplication.java
@@ -0,0 +1,13 @@
+package com.gil.arizon.juan.authorisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorisationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gil/arizon/juan/authorisation/AuthorisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gil.arizon.juan.authorisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
